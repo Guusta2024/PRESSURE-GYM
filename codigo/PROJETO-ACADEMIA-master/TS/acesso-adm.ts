f71fe2a5d421d73496a81a3f725f7cd77428b175
Guusta2024 <jose.gustavo1@escolar.ifrn.edu.br>
2025-03-11 10:22:24 -0300

    tratamento 2

--- codigo/PROJETO-ACADEMIA-master/TS/acesso-adm.ts
+++ codigo/PROJETO-ACADEMIA-master/TS/acesso-adm.ts
@@ -89,92 +89,111 @@ function excluirExercicio(index: number): void {
 
 
 function listarAlunos(): void {
-    const alunos = JSON.parse(localStorage.getItem("alunos") || "[]");
-    const tabela = document.getElementById("tabelaAlunos")?.querySelector("tbody");
-    if (tabela) {
-        tabela.innerHTML = "";
-        alunos.forEach((aluno: any, index: number) => {
-            const row = tabela.insertRow();
-            row.insertCell(0).textContent = aluno.nome;
-            row.insertCell(1).textContent = aluno.idade.toString();
-            row.insertCell(2).textContent = aluno.altura.toString();
-            row.insertCell(3).textContent = aluno.peso.toString();
-            row.insertCell(4).textContent = aluno.imc.toString();
-            row.insertCell(5).textContent = aluno.objetivo;
+    try{
+        const alunos = JSON.parse(localStorage.getItem("alunos") || "[]");
+        const tabela = document.getElementById("tabelaAlunos")?.querySelector("tbody");
+        if (tabela) {
+            tabela.innerHTML = "";
+            alunos.forEach((aluno: any, index: number) => {
+                const row = tabela.insertRow();
+                row.insertCell(0).textContent = aluno.nome;
+                row.insertCell(1).textContent = aluno.idade.toString();
+                row.insertCell(2).textContent = aluno.altura.toString();
+                row.insertCell(3).textContent = aluno.peso.toString();
+                row.insertCell(4).textContent = aluno.imc.toString();
+                row.insertCell(5).textContent = aluno.objetivo;
 
-            const deleteCell = row.insertCell(6);
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Excluir";
-            deleteButton.className = "btn-delete";
-            deleteButton.onclick = () => excluirAluno(index);
-            deleteCell.appendChild(deleteButton);
-        });
+                const deleteCell = row.insertCell(6);
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Excluir";
+                deleteButton.className = "btn-delete";
+                deleteButton.onclick = () => excluirAluno(index);
+                deleteCell.appendChild(deleteButton);
+            });
+        }
+    } catch (error){
+        console.error("erro ao listar alunos", error)
+        alert("ocorreu um erro ao listrar alunos")
     }
 }
 
 function listarInstrutores(): void {
     try{
-    const instrutores = JSON.parse(localStorage.getItem("instrutores") || "[]");
-    const tabela = document.getElementById("tabelaInstrutores")?.querySelector("tbody");
-    if (tabela) {
-        tabela.innerHTML = "";
-        instrutores.forEach((instrutor: any, index: number) => {
-            const row = tabela.insertRow();
-            row.insertCell(0).textContent = instrutor.nome;
-            row.insertCell(1).textContent = instrutor.idade.toString();
-            row.insertCell(2).textContent = instrutor.formacao;
-            row.insertCell(3).textContent = instrutor.email;
-            row.insertCell(4).textContent = instrutor.contato;
+        const instrutores = JSON.parse(localStorage.getItem("instrutores") || "[]");
+        const tabela = document.getElementById("tabelaInstrutores")?.querySelector("tbody");
+        if (tabela) {
+            tabela.innerHTML = "";
+            instrutores.forEach((instrutor: any, index: number) => {
+                const row = tabela.insertRow();
+                row.insertCell(0).textContent = instrutor.nome;
+                row.insertCell(1).textContent = instrutor.idade.toString();
+                row.insertCell(2).textContent = instrutor.formacao;
+                row.insertCell(3).textContent = instrutor.email;
+                row.insertCell(4).textContent = instrutor.contato;
 
-            const deleteCell = row.insertCell(5);
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Excluir";
-            deleteButton.className = "btn-delete";
-            deleteButton.onclick = () => excluirInstrutor(index);
-            deleteCell.appendChild(deleteButton);
-        });
+                const deleteCell = row.insertCell(5);
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Excluir";
+                deleteButton.className = "btn-delete";
+                deleteButton.onclick = () => excluirInstrutor(index);
+                deleteCell.appendChild(deleteButton);
+            });
+        }
+    } catch (error){
+        console.error("erro ao listar instrutores", error)
+        alert("ocorreu um erro ao listrar instrutores")
     }
 }
 function listarPlanosTreino(): void {
-    const planos = JSON.parse(localStorage.getItem("planosTreino") || "[]");
-    const tabela = document.getElementById("tabelaPlanosTreino")?.querySelector("tbody");
-    if (tabela) {
-        tabela.innerHTML = "";
-        planos.forEach((plano: any, index: number) => {
-            const row = tabela.insertRow();
-            row.insertCell(0).textContent = plano.nome;
-            row.insertCell(1).textContent = plano.descricao;
-            row.insertCell(2).textContent = plano.duracao.toString();
+    try{
+        const planos = JSON.parse(localStorage.getItem("planosTreino") || "[]");
+        const tabela = document.getElementById("tabelaPlanosTreino")?.querySelector("tbody");
+        if (tabela) {
+            tabela.innerHTML = "";
+            planos.forEach((plano: any, index: number) => {
+                const row = tabela.insertRow();
+                row.insertCell(0).textContent = plano.nome;
+                row.insertCell(1).textContent = plano.descricao;
+                row.insertCell(2).textContent = plano.duracao.toString();
 
-            const deleteCell = row.insertCell(3);
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Excluir";
-            deleteButton.className = "btn-delete";
-            deleteButton.onclick = () => excluirPlanoTreino(index);
-            deleteCell.appendChild(deleteButton);
-        });
+                const deleteCell = row.insertCell(3);
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Excluir";
+                deleteButton.className = "btn-delete";
+                deleteButton.onclick = () => excluirPlanoTreino(index);
+                deleteCell.appendChild(deleteButton);
+            });
+        }
+    } catch (error)  {
+        console.error("erro ao listar plano de treino", error)
+        alert("ocorreu um erro ao listrar plano de treino")
     }
 }
 
 function listarExercicios(): void {
-    const exercicios = JSON.parse(localStorage.getItem("exercicios") || "[]");
-    const tabela = document.getElementById("tabelaExercicios")?.querySelector("tbody");
-    if (tabela) {
-        tabela.innerHTML = "";
-        exercicios.forEach((exercicio: any, index: number) => {
-            const row = tabela.insertRow();
-            row.insertCell(0).textContent = exercicio.nome;
-            row.insertCell(1).textContent = exercicio.grupoMuscular;
-            row.insertCell(2).textContent = exercicio.repeticoes.toString();
-            row.insertCell(3).textContent = exercicio.carga.toString();
+    try{
+        const exercicios = JSON.parse(localStorage.getItem("exercicios") || "[]");
+        const tabela = document.getElementById("tabelaExercicios")?.querySelector("tbody");
+        if (tabela) {
+            tabela.innerHTML = "";
+            exercicios.forEach((exercicio: any, index: number) => {
+                const row = tabela.insertRow();
+                row.insertCell(0).textContent = exercicio.nome;
+                row.insertCell(1).textContent = exercicio.grupoMuscular;
+                row.insertCell(2).textContent = exercicio.repeticoes.toString();
+                row.insertCell(3).textContent = exercicio.carga.toString();
 
-            const deleteCell = row.insertCell(4);
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Excluir";
-            deleteButton.className = "btn-delete";
-            deleteButton.onclick = () => excluirExercicio(index);
-            deleteCell.appendChild(deleteButton);
-        });
+                const deleteCell = row.insertCell(4);
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Excluir";
+                deleteButton.className = "btn-delete";
+                deleteButton.onclick = () => excluirExercicio(index);
+                deleteCell.appendChild(deleteButton);
+            });
+        }
+    } catch (error){
+        console.error("erro ao listar execícios", error)
+        alert("ocorreu um erro ao listrar execícios")
     }
 }
 window.onload = () => {
